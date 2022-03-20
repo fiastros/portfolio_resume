989e761365641d4e89e3d79672bb8fba8d96186d
fiastros <loicyng@gmail.com>
2022-03-20 16:13:30 +0100

    Adding the flashing message

--- website/main_python.py
+++ website/main_python.py
@@ -1,16 +1,61 @@
-from flask import Flask, render_template, request, url_for, redirect
-from email.mime.text import MIMEText
+from flask import Flask, render_template, request, redirect, flash
 import smtplib
 from email.message import EmailMessage
+import json
+
+with open("confidential.json") as f:
+    confidential_info = json.load(f)
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "secret_key"
 
 @app.route("/")
 def home():
     return render_template("index.html")
 
+@app.route("/sendemail/", methods=["POST"])
+def sendemail():
+    if request.method == "POST":
+        #retrieving the information typed in the form by the user
+        sender_name = request.form["name"]
+        sender_subject = request.form["Subject"]
+        sender_email = request.form["_replyto"]
+        sender_message = request.form["message"]
+
+        #retrieving our personnal information from the confindential.json file
+        receiver_name = confidential_info.get("your_name")
+        receiver_email = confidential_info.get("your_email")
+        receiver_password = confidential_info.get("your_password")
+
+        #loggin in to our email account
+        server = smtplib.SMTP("smtp.gmail.com", 587)
+        server.ehlo()
+        server.starttls()
+        server.login(receiver_email,receiver_password)
+
+        #writing the mail
+        msg = EmailMessage()
+        msg.set_content("First Name: " + str(sender_name) +
+                        "\nEmail: " + str(sender_email) +
+                        "\nSubject: " + str(sender_subject) +
+                        "\n Message: " + str(sender_message)
+                        )
+        msg["Subject"] = "New Response on Personal Website"
+        msg["From"] = sender_email
+        msg["To"] = receiver_email
+
+        #Send the mail
+        try:
+            flash("Message. Expect an answer under 48 hour. Thank you !")
+            server.send_message(msg)
+        except:
+            flash(f"There has been an issue sending your mail. Please contact : {receiver_email}")
+            redirect("/")
+
+    return redirect("/")
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(threaded=True, debug=True, host = "127.0.0.1", port=5000)
 
 
 """
